9cf38cf1731fe0821e434dd5e30147d1a29d8af5
Stephen X. Flynn <stephenflynn@me.com>
2024-01-27 16:01:48 -0500

    first commit

---
+++ schooldb/src/main/java/com/flynn/schooldb/SchooldbApplication.java
@@ -0,0 +1,13 @@
+package com.flynn.schooldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooldbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchooldbApplication.class, args);
+	}
+
+}
---
+++ schooldb/src/test/java/com/flynn/schooldb/SchooldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flynn.schooldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
