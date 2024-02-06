b88ef9dea8c95d6bf9f35afc6b32b8bbc7fd6238
Stephen X. Flynn <stephenflynn@me.com>
2024-02-06 09:41:49 -0500

    remove pageable

--- schooldb/src/main/java/com/flynn/schooldb/repository/StudentRepository.java
+++ schooldb/src/main/java/com/flynn/schooldb/repository/StudentRepository.java
@@ -4,7 +4,6 @@ import com.flynn.schooldb.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
