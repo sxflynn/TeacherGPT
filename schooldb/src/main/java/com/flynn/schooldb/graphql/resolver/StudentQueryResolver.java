21cd640dc4e9e137c8226456e581abb7e7322540
Stephen Flynn <stephenflynn@me.com>
2024-02-08 08:48:15 -0500

    more graphql resolvers

---
+++ schooldb/src/main/java/com/flynn/schooldb/graphql/resolver/StudentQueryResolver.java
@@ -0,0 +1,30 @@
+package com.flynn.schooldb.graphql.resolver;
+import com.flynn.schooldb.entity.Student;
+import com.flynn.schooldb.service.StudentService;
+import com.flynn.schooldb.service.StudentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class StudentQueryResolver {
+    @Autowired
+    private StudentService studentService;
+
+    @QueryMapping
+    public Optional<Student> studentById(@Argument Long id) {
+        return studentService.getStudentById(id);
+    }
+
+    @QueryMapping
+    public List<Student> allStudents() {
+        return studentService.getAllStudents();
+    }
+}
+
+
+
