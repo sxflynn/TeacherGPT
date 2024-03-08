30df25c74e0c8b4887ea615088a8d27523fd31e1
Stephen Flynn <stephenflynn@me.com>
2024-03-08 18:54:51 -0500

    created healthcheck query

--- schooldb/src/main/java/com/flynn/schooldb/graphql/resolver/PingQueryResolver.java
+++ schooldb/src/main/java/com/flynn/schooldb/graphql/resolver/PingQueryResolver.java
@@ -1,2 +1,16 @@
-package com.flynn.schooldb.graphql.resolver;public class PingQueryResolver {
+package com.flynn.schooldb.graphql.resolver;
+import com.flynn.schooldb.entity.DailyAttendance;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class PingQueryResolver {
+
+    @QueryMapping
+    public String ping() {
+        return "Healthy";
+    }
 }
