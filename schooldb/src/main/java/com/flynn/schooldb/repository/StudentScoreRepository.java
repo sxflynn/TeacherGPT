222236e6f994874483c470690661e573350ef71b
Stephen Flynn <stephenflynn@me.com>
2024-04-12 16:24:09 -0400

    build: repository methods to assist the grade summary DTOs

--- schooldb/src/main/java/com/flynn/schooldb/repository/StudentScoreRepository.java
+++ schooldb/src/main/java/com/flynn/schooldb/repository/StudentScoreRepository.java
@@ -4,6 +4,7 @@ import com.flynn.schooldb.entity.StudentScore;
 import com.flynn.schooldb.entity.StudentScoreId;
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -26,5 +27,20 @@ public interface StudentScoreRepository extends JpaRepository<StudentScore, Stud
     List<StudentScore> findByAssignmentCourseCourseNameContainsIgnoreCaseAndAssignmentDateAssignedBetweenAndPercentageScoreLessThanOrderByAssignmentDateAssignedDesc(String courseName, LocalDate startDate, LocalDate endDate, Float threshold);
 
     List<StudentScore> findByAssignmentCourseCourseNameContainsIgnoreCaseAndAssignmentDateAssignedBetweenAndMissingIsTrueOrderByAssignmentDateAssignedDesc(String courseName, LocalDate startDate, LocalDate endDate);
+
+
+    // CourseGradeDTO repository methods
+
+    // count all grades between dates
+    @Query("SELECT COUNT(ss) FROM StudentScore ss WHERE ss.student.studentId = :studentId AND LOWER(ss.assignment.course.courseName) LIKE LOWER(CONCAT('%', :courseName, '%')) AND ss.assignment.dateAssigned BETWEEN :startDate AND :endDate")
+    Long countByStudentIdAndCourseNameLikeAndDateAssignedBetween(Long studentId, String courseName, LocalDate startDate, LocalDate endDate);
+
+    // sum the points earned
+    @Query("SELECT SUM(ss.pointsEarned) FROM StudentScore ss WHERE ss.student.studentId = :studentId AND LOWER(ss.assignment.course.courseName) LIKE LOWER(CONCAT('%', :courseName, '%')) AND ss.assignment.dateAssigned BETWEEN :startDate AND :endDate")
+    Long sumPointsEarnedByStudentAndCourseBetweenDates(Long studentId, String courseName, LocalDate startDate, LocalDate endDate);
+
+    // sum the total values
+    @Query("SELECT SUM(ss.assignment.assignmentValue) FROM StudentScore ss WHERE ss.student.studentId = :studentId AND LOWER(ss.assignment.course.courseName) LIKE LOWER(CONCAT('%', :courseName, '%')) AND ss.assignment.dateAssigned BETWEEN :startDate AND :endDate")
+    Long sumAssignmentValueByStudentAndCourseBetweenDates(Long studentId, String courseName, LocalDate startDate, LocalDate endDate);
 
 }
