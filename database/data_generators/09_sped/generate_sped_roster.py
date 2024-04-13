5aabb6b5fa5312f2d44c4930ea830c7cc2dd7d86
Stephen Flynn <stephenflynn@me.com>
2024-04-13 14:43:16 -0400

    sped data generator first commit

---
+++ database/data_generators/09_sped/generate_sped_roster.py
@@ -0,0 +1,21 @@
+student_csv_file_name = '../03/students/csv'
+
+# https://education.ohio.gov/getattachment/About/Annual-Reports/Special-Education-Implementation-Legislative-Report-SY21-22.pdf
+
+PERCENTAGE_OF_STUDENTS_IN_SPED = .16
+
+CATEGORY_PROBABILITIES = {
+    "specific_learning_disabilities": 0.3518,
+    "other_health_impaired": 0.1880,
+    "speech_and_language": 0.1375,
+    "autism": 0.1169,
+    "intellectual_disabilities": 0.0666,
+    "emotional_disturbance": 0.0499,
+    "multiple_disabilities": 0.0466,
+    "developmental_delay": 0.0231,
+    "hearing_impairments": 0.0074,
+    "traumatic_brain_injury": 0.0055,
+    "orthopedic_impairments": 0.0051,
+    "visual_impairments": 0.0034,
+    "deaf_blindness": 0.0003
+}
